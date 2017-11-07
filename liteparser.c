e979aaf41064ebc07a71472f013e6744b1ee6cc7
np1802 <32307466+np1802@users.noreply.github.com>
2017-11-07 17:56:41 +0100

    Blocksize checked

--- liteparser.c
+++ liteparser.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<stdint.h>
+#include <openssl/evp.h>
+#include <openssl/sha.h>
+#include <time.h>
+
 int Magic_number(FILE* BLOCK)
 {
 	unsigned char magic_check[4] = {0xfb,0xc0,0xb6,0xdb};
@@ -34,6 +39,37 @@ int Magic_number(FILE* BLOCK)
 	}
 }
 
+int calculate_block_size(FILE* BLOCK)
+{
+	uint32_t block_size = 0;
+	int count=fread(&block_size, 1, 4, BLOCK);
+	if (count!=4)
+	{
+		printf("\nError reading block size\n");
+	}
+	
+	printf("Block_size is : %u\n",block_size);
+
+	/*To calculate block size*/
+	
+    uint32_t block_start_pointer = 0;
+	block_start_pointer = ftell(BLOCK);  //where is file pointer righ now save it 
+	fseek(BLOCK, 0, SEEK_END);          // placing the file pointer to end of file
+	int cal_size_of_BLOCK = ftell(BLOCK);      // determining the size of plain text file
+	
+	int actual_blk_size = cal_size_of_BLOCK -  block_start_pointer ; //size in field is sizeof_file - 8bytes(4 bytes magic, 4 bytes size)
+	
+	if (actual_blk_size!= block_size)
+	{
+		printf("Incorrect block size");
+		return 1;
+	}
+	
+	fseek(BLOCK, block_start_pointer, SEEK_SET);          // placing the file pointer again to its previous position (8 byte from start)
+	return 0;
+	
+}
+
 int main()
 {
 	FILE *BLOCK;
@@ -52,6 +88,16 @@ int main()
 	else
 	{
 		printf("\nMagic Number invalid for Litecoin ");
+	}
+	
+	retval=calculate_block_size(BLOCK);
+	if (retval==0)
+	{
+		printf("\nBlock Size found valid");
+	}
+	else
+	{
+		printf("\nBlock Size not found valid");
 	}
 	return 0;
 }
